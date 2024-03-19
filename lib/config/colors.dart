63a206e739aba2a538d903fb9370712668b13949
Hemthath <hemthath.s@datasirpi.com>
2024-03-19 13:33:46 +0530

    initial commit of new delivery app

---
+++ lib/config/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = Color(0xffd1ad17);
+Color scaffoldBackgroundColor = Color(0xffcbcbcb);
+Color textColor = Colors.black87;
+
+class AppConstants {
+  static String newMember = "New Member";
+  static String alreadyMember = "Already member";
+  static String register = "Register";
+  static String login = "Login";
+  static String loginTitle = "loginTitle";
+  static String loginTitle2 = "Welcome back";
+  static String signupTitle2 = "sign to access your account";
+  static String signupTitle = "Get Started";
+  static String next = "Next";
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:food_app/auth/sign_in.dart';
+import 'package:food_app/config/colors.dart';
+import 'package:food_app/providers/check_out_provider.dart';
+import 'package:food_app/providers/product_provider.dart';
+import 'package:food_app/providers/review_cart_provider.dart';
+import 'package:food_app/providers/user_provider.dart';
+import 'package:food_app/providers/wishlist_provider.dart';
+import 'package:food_app/screens/home/home_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyDVLsiV0geFAknw8w9F8IK6lp_Ha5aMjdU",
+      // authDomain: "YOUR_AUTH_DOMAIN",
+      projectId: "delivery-app-e225c",
+      storageBucket: "delivery-app-e225c.appspot.com",
+      messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+      appId: "1:656885821838:android:e7fc6718a4f8ac780a34df",
+    ),
+  );
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ProductProvider>(
+          create: (context) => ProductProvider(),
+        ),
+        ChangeNotifierProvider<UserProvider>(
+          create: (context) => UserProvider(),
+        ),
+        ChangeNotifierProvider<ReviewCartProvider>(
+          create: (context) => ReviewCartProvider(),
+        ),
+        ChangeNotifierProvider<WishListProvider>(
+          create: (context) => WishListProvider(),
+        ),
+        ChangeNotifierProvider<CheckoutProvider>(
+          create: (context) => CheckoutProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+            primaryColor: primaryColor,
+            scaffoldBackgroundColor: scaffoldBackgroundColor),
+        debugShowCheckedModeBanner: false,
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapShot) {
+            if (snapShot.hasData) {
+              return HomeScreen();
+            }
+            return SiginIn();
+          },
+        ),
+      ),
+    );
+  }
+}
